447d27ac87821b9dc1da66426a13d43b25caeaf6
Jay <jay.17984@sakec.ac.in>
2026-03-08 18:44:40 +0530

    User Authentication is completed, tested it using Postman

---
+++ server/controllers/authController.js
@@ -0,0 +1,55 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+const generateToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET);
+};
+
+const register = async (req,res) => {
+    try {
+        const {username, email, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+        if (userExist) return res.status(400).json({message: "Email already in use"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User.create({ username, email, password: hashedPassword });
+
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } catch(err){
+        res.status(500).json({ message: `Error occured: ${err.message}`});
+    }
+};
+
+const login = async (req,res) => {
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({ message: "Invalid Password" });
+
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } catch(err) {
+        res.status(500).json({ message: `Error occured: ${err.message}`});
+    }
+};
+
+module.exports = { register, login };
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
 dotenv.config();
+
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
 connectDB();
 
 const app = express()
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/',(req,res) => {
     res.send("The Server is running!");
